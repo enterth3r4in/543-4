85f94f3c6e1cb9524d0c3b365c163451d56850ad
enterth3r4in <believe.the.rain@gmail.com>
2015-12-08 14:11:55 -0500

    Clear Button Functioning

--- Page543-4/Page543-4/Form1.vb
+++ Page543-4/Page543-4/Form1.vb
@@ -4,4 +4,24 @@
         'End Application
         Application.Exit()
     End Sub
+
+    Private Sub buttonClearForm_Click(sender As Object, e As EventArgs) Handles buttonClearForm.Click
+        ' Clear all radio buttons, check boxes and total box
+        For Each rdoBtn As RadioButton In groupBoxScoops.Controls
+            rdoBtn.Checked = False
+        Next
+        For Each rdoBtn As RadioButton In groupBoxFlavors.Controls
+            rdoBtn.Checked = False
+        Next
+        For Each chkBox As CheckBox In groupBoxAdditions.Controls
+            chkBox.Checked = False
+        Next
+        lblScoopCostOutput.Text = String.Empty
+        lblAdditionCostOutput.Text = String.Empty
+        lblTotalCostOutput.Text = String.Empty
+    End Sub
+
+    Private Sub buttonOrderNow_Click(sender As Object, e As EventArgs) Handles buttonOrderNow.Click
+
+    End Sub
 End Class

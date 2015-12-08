dd800efeb55ebba4527a4a7ecb16096eb8fe6b39
enterth3r4in <believe.the.rain@gmail.com>
2015-12-08 13:59:54 -0500

    Exit program working

--- Page543-4/Page543-4/Form1.vb
+++ Page543-4/Page543-4/Form1.vb
@@ -1,3 +1,7 @@
 ﻿Public Class Form1
 
+    Private Sub buttonExitProgram_Click(sender As Object, e As EventArgs) Handles buttonExitProgram.Click
+        'End Application
+        Application.Exit()
+    End Sub
 End Class

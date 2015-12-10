038e6b8d6512dba64fbd5822cf4cbee06a5b96ef
enterth3r4in <believe.the.rain@gmail.com>
2015-12-10 13:53:01 -0500

    All functinality working

--- Page543-4/Page543-4/Form1.vb
+++ Page543-4/Page543-4/Form1.vb
@@ -33,7 +33,10 @@
     End Sub
 
     Private Sub buttonOrderNow_Click(sender As Object, e As EventArgs) Handles buttonOrderNow.Click
-
+        dblTotal = dblScoopsTotal + dblAdditionsTotal
+        lblScoopCostOutput.Text = dblScoopsTotal.ToString("c")
+        lblAdditionCostOutput.Text = dblAdditionsTotal.ToString("c")
+        lblTotalCostOutput.Text = dblTotal.ToString("c")
     End Sub
 
     Private Sub rdoOneScoop_CheckedChanged(sender As Object, e As EventArgs) Handles rdoOneScoop.CheckedChanged
@@ -46,5 +49,45 @@
 
     Private Sub rdoThreeScoop_CheckedChanged(sender As Object, e As EventArgs) Handles rdoThreeScoop.CheckedChanged
         dblScoopsTotal = If(rdoThreeScoop.Checked, dblTripleScoop, 0D)
+    End Sub
+
+    Private Sub chkPeanuts_CheckedChanged(sender As Object, e As EventArgs) Handles chkPeanuts.CheckedChanged
+        dblAdditionsTotal += If(chkPeanuts.Checked, dblAdditions, -dblAdditions)
+    End Sub
+
+    Private Sub chkStrawberries_CheckedChanged(sender As Object, e As EventArgs) Handles chkStrawberries.CheckedChanged
+        dblAdditionsTotal += If(chkStrawberries.Checked, dblAdditions, -dblAdditions)
+    End Sub
+
+    Private Sub chkWhipCream_CheckedChanged(sender As Object, e As EventArgs) Handles chkWhipCream.CheckedChanged
+        dblAdditionsTotal += If(chkWhipCream.Checked, dblAdditions, -dblAdditions)
+    End Sub
+
+    Private Sub chkCherries_CheckedChanged(sender As Object, e As EventArgs) Handles chkCherries.CheckedChanged
+        dblAdditionsTotal += If(chkCherries.Checked, dblAdditions, -dblAdditions)
+    End Sub
+
+    Private Sub chkBrownieChunk_CheckedChanged(sender As Object, e As EventArgs) Handles chkBrownieChunk.CheckedChanged
+        dblAdditionsTotal += If(chkBrownieChunk.Checked, dblAdditions, -dblAdditions)
+    End Sub
+
+    Private Sub chkCookieDough_CheckedChanged(sender As Object, e As EventArgs) Handles chkCookieDough.CheckedChanged
+        dblAdditionsTotal += If(chkCookieDough.Checked, dblAdditions, -dblAdditions)
+    End Sub
+
+    Private Sub chkHotFudge_CheckedChanged(sender As Object, e As EventArgs) Handles chkHotFudge.CheckedChanged
+        dblAdditionsTotal += If(chkHotFudge.Checked, dblAdditions, -dblAdditions)
+    End Sub
+
+    Private Sub chkKiwi_CheckedChanged(sender As Object, e As EventArgs) Handles chkKiwi.CheckedChanged
+        dblAdditionsTotal += If(chkKiwi.Checked, dblAdditions, -dblAdditions)
+    End Sub
+
+    Private Sub chkMMS_CheckedChanged(sender As Object, e As EventArgs) Handles chkMMS.CheckedChanged
+        dblAdditionsTotal += If(chkMMS.Checked, dblAdditions, -dblAdditions)
+    End Sub
+
+    Private Sub chkOreo_CheckedChanged(sender As Object, e As EventArgs) Handles chkOreo.CheckedChanged
+        dblAdditionsTotal += If(chkOreo.Checked, dblAdditions, -dblAdditions)
     End Sub
 End Class
